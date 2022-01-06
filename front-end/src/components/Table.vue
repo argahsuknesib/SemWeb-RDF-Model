1b61b27e4d959d51e1cf39ac2b2df32ee7718397
Kushagra Singh BISEN <k.bisen@yahoo.com>
2022-01-06 23:29:29 +0100

    implementation of frontend

--- front-end/src/components/Table.vue
+++ front-end/src/components/Table.vue
@@ -1,54 +1,33 @@
 <template>
+  <table
+    id="example"
+    class="table table-striped table-bordered"
+    style="width: 100%"
+  >
+    <thead>
+      <tr>
+        <th v-for="key in columns" v-bind:key="key">{{ key }}</th>
+      </tr>
+    </thead>
 
+    <tbody>
+      <tr v-for="random in knowledge" v-bind:key="random">
+        <td>{{ random.subject.value }}</td>
+        <!-- <td>{{ random.subject.type }}</td> -->
+        <td>{{ random.predicate.value }}</td>
+        <!-- <td>{{ random.predicate.type }}</td> -->
+        <td>{{ random.object.value }}</td>
+        <!-- <td>{{ random.object.type }}</td> -->
 
-  <v-card>
-    <v-card-title>
-      Nutrition
-      <v-spacer></v-spacer>
-      <v-text-field
-        v-model="search"
-        append-icon="mdi-magnify"
-        label="Search"
-        single-line
-        hide-details
-      ></v-text-field>
-    </v-card-title>
-    <v-data-table
-      :headers="headers"
-      :items="desserts"
-      :search="search"
-    ></v-data-table>
-  </v-card>
-<!-- <div>
-    {{knowledge.results.bindings[0].subject.value}}
-    <hr>
-    {{knowledge.results.bindings[0].predicate.value}}
-    <hr>
-    {{knowledge.results.bindings[0].object.value}}
+      </tr>
+    </tbody>
 
-</div> -->
-  <!-- <div class="table">
-    <div class="container mb-3 mt-3">
-      <table
-        class="table table-striped table-bordered mydatatable"
-        style="width: 100%"
-      >
-        <thead>
-          <tr>
-            <th>Subject</th>
-            <th>Predicate</th>
-            <th>Object</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr v-for="values in this.knowledge" v-bind:key="values.id">
-              <td></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>-->
+    <tfoot>
+      <tr>
+        <th v-for="key in columns" v-bind:key="key">{{ key }}</th>
+      </tr>
+    </tfoot>
+  </table>
 </template>
 
 <script>
@@ -59,15 +38,14 @@ export default {
   data() {
     return {
       knowledge: [],
-      columns: ["Subject", "Predicate", "Object"],
+      columns: ["Subject","Predicate","Object"],
     };
   },
   created: async function () {
-    let response =  await axios.get(`http://localhost:3000/knowledge`);
+    let response = await axios.get(`http://localhost:3000/knowledge`);
     console.log(response.data);
-    let knowledge = response.data;
-    this.knowledge =  knowledge;
+    let knowledge = response.data.results.bindings;
+    this.knowledge = knowledge;
   },
 };
-
 </script>
